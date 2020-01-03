961b2120ef34605e06df87a99f9cfacc4d0b4563
Scott Ratigan <scotteratigan@gmail.com>
2020-01-03 08:30:01 -0800

    context working in unorthadox fashion

--- src/Components/List.tsx
+++ src/Components/List.tsx
@@ -1,33 +1,14 @@
-import React, { useContext, useState } from 'react'
+import React, { useContext } from 'react'
 import TodoContext from '../TodoContext'
 import styled from 'styled-components'
 import AddTodo from './AddTodo'
 
-type TodoLiProps = {
-  complete: boolean
-}
-
-type TodoTypes = {
-  name: string
-  complete: boolean
-}
-
 export default function List() {
-  const { todos } = useContext(TodoContext)
-  console.log('todos:', todos)
-  const toggleTodoCompleted = (i: number) => {
-    // const newTodos = [...todos]
-    // newTodos[i].complete = !newTodos[i].complete
-    // setTodos(newTodos)
-  }
-  const addTodo = (name: string) => {
-    // const newTodos = [...todos, { name, complete: false }]
-    // setTodos(newTodos)
-  }
-  const removeTodo = (i: number) => {
-    // const newTodos = [...todos]
-    // newTodos.splice(i, 1)
-    // setTodos(newTodos)
+  const { todos, toggleTodoCompleted, removeTodo } = useContext(TodoContext)
+
+  type TodoTypes = {
+    name: string
+    complete: boolean
   }
 
   return (
@@ -43,7 +24,8 @@ export default function List() {
         </TodoList>
       </FlexCenterContainer>
       <FlexCenterContainer>
-        <AddTodo addTodoFn={addTodo} />
+        {/* <AddTodo addTodoFn={addTodo} /> */}
+        <AddTodo />
       </FlexCenterContainer>
     </>
   )
@@ -53,7 +35,7 @@ const TodoList = styled.ul`
   list-style-type: none;
 `
 
-const TodoItem = styled.li<TodoLiProps>`
+const TodoItem = styled.li<{ complete: boolean }>`
   padding: 1rem;
   color: ${p => (p.complete ? 'grey' : 'black')};
   text-decoration: ${p => (p.complete ? 'line-through' : 'none')};
