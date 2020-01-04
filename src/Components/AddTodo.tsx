9bd9127fe00d8df048b86f8b975b59b5cf26bc4c
Scott Ratigan <scotteratigan@gmail.com>
2020-01-04 05:53:07 -0800

    Enter key now submits.

--- src/Components/AddTodo.tsx
+++ src/Components/AddTodo.tsx
@@ -1,6 +1,7 @@
 import React, { useContext, useState } from 'react'
 import TodoContext from '../TodoContext'
 import { PrimaryButton } from './Buttons'
+import { checkServerIdentity } from 'tls'
 
 export default function AddTodo() {
   const [todoText, setTodoText] = useState('')
@@ -10,12 +11,16 @@ export default function AddTodo() {
     addTodo(todoText)
     setTodoText('')
   }
+  const checkToSubmit = (key: string) => {
+    if (key === 'Enter') addTodoToList()
+  }
   return (
     <div>
       <input
         type="text"
         value={todoText}
         onChange={e => setTodoText(e.target.value)}
+        onKeyDown={e => checkToSubmit(e.key)}
       ></input>
       <PrimaryButton onClick={addTodoToList}>Add Todo</PrimaryButton>
     </div>
